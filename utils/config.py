d30fb22e467e1f2a04e6b14f9c5e08974af2400e
imporatore <48373375+imporatore@users.noreply.github.com>
2021-01-20 16:15:39 +0800

    update esg test

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import os
+
+__proj_dir = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = r'D:\QuantData'
+
+# Tushare PRO API token
+USER_TOKEN = 'eca945725d5e00bdcc41fd98242a723f6f5199649612f9483cd4cfd1'
+
+
---
+++ utils/test_portfolio.py
@@ -0,0 +1,12 @@
+import alphalens
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+
+
