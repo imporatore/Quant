47902d2b7abb1af538fa4675065af4a40939acce
imporatore <48373375+imporatore@users.noreply.github.com>
2021-01-26 09:59:33 +0800

    fix bugs

--- utils/test_portfolio.py
+++ utils/test_portfolio.py
@@ -3,10 +3,4 @@ import alphalens
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
-
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
-
-
-
+from utils.plot import *
